6612f0a8237ae91e77b8cc2ce9f14afcb697ae14
Michał Wieluński <michal@w-ski.dev>
2018-06-24 14:23:23 +0200

    add exception alerts

--- src/Fnake/Main.java
+++ src/Fnake/Main.java
@@ -1,11 +1,16 @@
 package Fnake;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -17,7 +22,7 @@ import java.io.*;
 public class Main extends Application {
 
     static HighScore scores;
-    private String saveName = "highScore.save";
+    private static String saveName = "highScore.save";
     static String name = "Anonymous";
 
     public static void main(String[] args) {
@@ -44,6 +49,33 @@ public class Main extends Application {
         } else {
             return new HighScore();
         }
+    }
+
+    private static void showError(Thread t, Throwable e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Unexpected Exception");
+        alert.setContentText(e.toString());
+
+        alert.showAndWait();
+        exitGame();
+    }
+
+    private static void exitGame() {
+        FileOutputStream fout;
+        try {
+            fout = new FileOutputStream(saveName);
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            oos.writeObject(scores);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("High scores could not be saved.");
+            alert.setContentText(e.toString());
+
+            alert.showAndWait();
+        }
+        System.exit(0);
     }
 
     @Override
@@ -83,6 +115,22 @@ public class Main extends Application {
         primaryStage.show();
 
         scores = loadHighScores();
+        Thread.setDefaultUncaughtExceptionHandler(Main::showError);
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            final KeyCombination keyComb = new KeyCodeCombination(
+                    KeyCode.Q,
+                    KeyCombination.SHIFT_DOWN,
+                    KeyCombination.META_DOWN
+            );
+
+            public void handle(KeyEvent ke) {
+                if (keyComb.match(ke)) {
+                    //noinspection NumericOverflow,divzero
+                    int var = 2 / 0; // sanity check
+                }
+            }
+        });
 
         newGame.setOnAction(event -> {
             Result result = NewGameDialog.createDialog();
@@ -97,26 +145,8 @@ public class Main extends Application {
             }
         });
 
-        highScores.setOnAction(event -> {
-            HighScoreDialog.createDialog();
-        });
+        highScores.setOnAction(event -> HighScoreDialog.createDialog());
 
-        exitGame.setOnAction(event -> {
-
-            FileOutputStream fout;
-            try {
-                fout = new FileOutputStream(saveName);
-                ObjectOutputStream oos = new ObjectOutputStream(fout);
-                oos.writeObject(scores);
-            } catch (IOException e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("High scores could not be saved.");
-                alert.setContentText(e.toString());
-
-                alert.showAndWait();
-            }
-            System.exit(0);
-        });
+        exitGame.setOnAction(event -> exitGame());
     }
 }
